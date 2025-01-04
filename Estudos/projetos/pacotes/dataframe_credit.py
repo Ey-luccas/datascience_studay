3eb59eb9f673fc373ebb98a93b47812981313270
eyluccas <eyluccas@gmail.com>
2025-01-04 16:15:58 -0300

    Ajuste no reposítorio

--- Estudos/projetos/pacotes/dataframe_credit.py
+++
@@ -1,4 +0,0 @@
-def dataframe_credit():
-  import pandas as pd
-  dataset = pd.read_csv("C:/Users/eyluc/Documents/ESTUDO -- 'data scientist'/Eydatascience/Estudos/Datasets/csv/credit_dt.csv")
-  return dataset
